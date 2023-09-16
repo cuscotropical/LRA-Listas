14ebce2f6763aa5fbb5fdac7d0a9c07cef9a053e
Leandro Rodrigues Alexandre <83886885+cuscotropical@users.noreply.github.com>
2023-09-16 16:56:44 +0100

    update usando jquery
    Foi feito uma troca de partes do js para jquery

--- main.js
+++ main.js
@@ -1,85 +1,95 @@
-    const texto = document.querySelector('input')
-    const btnInsert = document.querySelector('.divInsert button')
-    const btnDeleteAll = document.querySelector('.header button')
-    const ul = document.querySelector('ul')
-
-    var itensDB = []
-
-    btnDeleteAll.onclick = () => {
-    itensDB = []
-    updateDB()
-    }
-
-    texto.addEventListener('keypress', e => {
-    if (e.key == 'Enter' && texto.value != '') {
-        setItemDB()
-    }
-    })
-
-    btnInsert.onclick = () => {
-    if (texto.value != '') {
-        setItemDB()
-    }
-    }
-
-    function setItemDB() {
-    if (itensDB.length >= 20) {
-        alert('Limite máximo de 20 itens atingido!')
-        return
-    }
-
-    itensDB.push({ 'item': texto.value, 'status': '' })
-    updateDB()
-    }
-
-    function updateDB() {
-    localStorage.setItem('todolist', JSON.stringify(itensDB))
-    loadItens()
-    }
-
-    function loadItens() {
-    ul.innerHTML = "";
-    itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
-    itensDB.forEach((item, i) => {
-        insertItemTela(item.item, item.status, i)
-    })
-    }
-
-    function insertItemTela(text, status, i) {
-    const li = document.createElement('li')
+    $(document).ready(function () {
+        const texto = $('input')
+        const btnInsert = $('.divInsert button')
+        const btnDeleteAll = $('.header button')
+        const ul = $('ul')
     
-    li.innerHTML = `
-        <div class="divLi">
-        <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
-        <span data-si=${i}>${text}</span>
-        <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
-        </div>
-        `
-    ul.appendChild(li)
-
-    if (status) {
-        document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
-    } else {
-        document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
-    }
-
-    texto.value = ''
-    }
-
-    function done(chk, i) {
-
-    if (chk.checked) {
-        itensDB[i].status = 'checked' 
-    } else {
-        itensDB[i].status = '' 
-    }
-
-    updateDB()
-    }
-
-    function removeItem(i) {
-    itensDB.splice(i, 1)
-    updateDB()
-    }
-
-    loadItens()
+        var itensDB = []
+    
+        btnDeleteAll.on('click', function () {
+        itensDB = []
+        updateDB()
+        })
+    
+        texto.on('keypress', function (e) {
+        if (e.key === 'Enter' && texto.val() !== '') {
+            setItemDB()
+        }
+        })
+    
+        btnInsert.on('click', function () {
+        if (texto.val() !== '') {
+            setItemDB()
+        }
+        })
+    
+        function setItemDB() {
+        if (itensDB.length >= 20) {
+            alert('Limite máximo de 20 itens atingido!')
+            return
+        }
+    
+        itensDB.push({ item: texto.val(), status: '' })
+        updateDB()
+        }
+    
+        function updateDB() {
+        localStorage.setItem('todolist', JSON.stringify(itensDB))
+        loadItens()
+        }
+    
+        function loadItens() {
+        ul.html('')
+        itensDB = JSON.parse(localStorage.getItem('todolist')) || []
+        itensDB.forEach(function (item, i) {
+            insertItemTela(item.item, item.status, i)
+        })
+        }
+    
+        function insertItemTela(text, status, i) {
+        const li = $('<li></li>')
+    
+        li.html(`
+            <div class="divLi">
+            <input type="checkbox" ${status} data-i=${i} />
+            <span data-si=${i}>${text}</span>
+            <button data-i=${i}><i class='bx bx-trash'></i></button>
+            </div>
+        `)
+        ul.append(li)
+    
+        if (status === 'checked') {
+            $(`[data-si="${i}"]`).addClass('line-through')
+            li.find('input').prop('checked', true); // Marcar a caixa de seleção se estiver completo
+        }
+    
+        li.find('input').on('change', function () {
+            done(this, i);
+        });
+    
+        li.find('button').on('click', function () {
+            removeItem(i);
+        });
+    
+        texto.val('')
+        }
+    
+        function done(chk, i) {
+        if (chk.checked) {
+            itensDB[i].status = 'checked'
+            $(`[data-si="${i}"]`).addClass('line-through')
+        } else {
+            itensDB[i].status = ''
+            $(`[data-si="${i}"]`).removeClass('line-through')
+        }
+    
+        updateDB()
+        }
+    
+        function removeItem(i) {
+        itensDB.splice(i, 1)
+        updateDB()
+        }
+    
+        loadItens()
+    })
